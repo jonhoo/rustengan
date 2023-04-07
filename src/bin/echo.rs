f3382bcc022fee1b896522418fa6a84391e70849
Jon Gjengset <jon@thesquareplanet.com>
2023-04-07 10:52:10 -0700

    Split out shareable parts

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,24 +1,8 @@
-use std::io::{StdoutLock, Write};
+use rustengan::*;
 
 use anyhow::{bail, Context};
 use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Message {
-    src: String,
-    #[serde(rename = "dest")]
-    dst: String,
-    body: Body,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Body {
-    #[serde(rename = "msg_id")]
-    id: Option<usize>,
-    in_reply_to: Option<usize>,
-    #[serde(flatten)]
-    payload: Payload,
-}
+use std::io::{StdoutLock, Write};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
@@ -41,8 +25,8 @@ struct EchoNode {
     id: usize,
 }
 
-impl EchoNode {
-    pub fn step(&mut self, input: Message, output: &mut StdoutLock) -> anyhow::Result<()> {
+impl Node<Payload> for EchoNode {
+    fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
             Payload::Init { .. } => {
                 let reply = Message {
@@ -82,19 +66,5 @@ impl EchoNode {
 }
 
 fn main() -> anyhow::Result<()> {
-    let stdin = std::io::stdin().lock();
-    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
-
-    let mut stdout = std::io::stdout().lock();
-
-    let mut state = EchoNode { id: 0 };
-
-    for input in inputs {
-        let input = input.context("Maelstrom input from STDIN could not be deserialized")?;
-        state
-            .step(input, &mut stdout)
-            .context("Node step function failed")?;
-    }
-
-    Ok(())
+    main_loop(EchoNode { id: 0 })
 }
---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use anyhow::Context;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::io::StdoutLock;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message<Payload> {
+    pub src: String,
+    #[serde(rename = "dest")]
+    pub dst: String,
+    pub body: Body<Payload>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Body<Payload> {
+    #[serde(rename = "msg_id")]
+    pub id: Option<usize>,
+    pub in_reply_to: Option<usize>,
+    #[serde(flatten)]
+    pub payload: Payload,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Init {
+    pub node_id: String,
+    pub node_ids: Vec<String>,
+}
+
+pub trait Node<Payload> {
+    fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()>;
+}
+
+pub fn main_loop<S, Payload>(mut state: S) -> anyhow::Result<()>
+where
+    S: Node<Payload>,
+    Payload: DeserializeOwned,
+{
+    let stdin = std::io::stdin().lock();
+    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message<Payload>>();
+
+    let mut stdout = std::io::stdout().lock();
+
+    for input in inputs {
+        let input = input.context("Maelstrom input from STDIN could not be deserialized")?;
+        state
+            .step(input, &mut stdout)
+            .context("Node step function failed")?;
+    }
+
+    Ok(())
+}
