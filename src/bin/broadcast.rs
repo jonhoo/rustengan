eba0f97a37b7a9e3aeb6f890c9ea18e33c888c20
Jon Gjengset <jon@thesquareplanet.com>
2023-04-07 11:51:13 -0700

    Single-node broadcast

---
+++ src/bin/broadcast.rs
@@ -0,0 +1,75 @@
+use rustengan::*;
+
+use anyhow::Context;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    io::{StdoutLock, Write},
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Broadcast {
+        message: usize,
+    },
+    BroadcastOk,
+    Read,
+    ReadOk {
+        messages: Vec<usize>,
+    },
+    Topology {
+        topology: HashMap<String, Vec<String>>,
+    },
+    TopologyOk,
+}
+
+struct BroadcastNode {
+    node: String,
+    id: usize,
+    messages: Vec<usize>,
+}
+
+impl Node<(), Payload> for BroadcastNode {
+    fn from_init(_state: (), init: Init) -> anyhow::Result<Self> {
+        Ok(Self {
+            node: init.node_id,
+            id: 1,
+            messages: Vec::new(),
+        })
+    }
+
+    fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
+        let mut reply = input.into_reply(Some(&mut self.id));
+        match reply.body.payload {
+            Payload::Broadcast { message } => {
+                self.messages.push(message);
+                reply.body.payload = Payload::BroadcastOk;
+                serde_json::to_writer(&mut *output, &reply)
+                    .context("serialize response to broadcast")?;
+                output.write_all(b"\n").context("write trailing newline")?;
+            }
+            Payload::Read => {
+                reply.body.payload = Payload::ReadOk {
+                    messages: self.messages.clone(),
+                };
+                serde_json::to_writer(&mut *output, &reply)
+                    .context("serialize response to read")?;
+                output.write_all(b"\n").context("write trailing newline")?;
+            }
+            Payload::Topology { topology: _ } => {
+                reply.body.payload = Payload::TopologyOk;
+                serde_json::to_writer(&mut *output, &reply)
+                    .context("serialize response to topology")?;
+                output.write_all(b"\n").context("write trailing newline")?;
+            }
+            Payload::BroadcastOk | Payload::ReadOk { .. } | Payload::TopologyOk => {}
+        }
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    main_loop::<_, BroadcastNode, _>(())
+}
